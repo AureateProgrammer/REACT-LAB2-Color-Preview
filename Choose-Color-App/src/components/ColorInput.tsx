03cb236d69ab37f2d0532262a320cf32396e61e9
AureteProgrammer <CodingBlakeMorris@gmail.com>
2026-04-23 15:57:14 -0400

    Creating comments

--- Choose-Color-App/src/components/ColorInput.tsx
+++ Choose-Color-App/src/components/ColorInput.tsx
@@ -2,16 +2,24 @@ import { useState } from 'react';
 
 
 type ColorInputProps = {
+    //this is a function coming from thje parent component
+    //It receives the color text the user types
   onColorChange: (color: string) => void;
 };
 
 function ColorInput({ onColorChange }: ColorInputProps) {
+    //this runs whenever the user types in the input
   return (
     <input
       type="text"
-      onChange={(event) => onColorChange(event.target.value)}
+      onChange={(event) =>
+        // Send the typeed value back to the parent component 
+        onColorChange(event.target.value)}
     />
   );
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    onColorChange(event.target.value);
+  }
 }
 
 export default ColorInput;
